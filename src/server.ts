2b9365167579f80d899464fecf1e3aa80a113210
Vitor Maia <vitor_maia_silva@hotmail.com>
2022-09-17 23:12:22 -0300

    tratativa para rotas não encontradas

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import AdsRoute from "./routes/ads";
 import GamesRoute from "./routes/games";
 import cors from 'cors'
+import HttpCodes from "./constants/HttpCodes";
 const app = express()
 
 app.use(cors())
@@ -9,6 +10,8 @@ app.use(cors())
 app.use(express.json())
 app.use('/ads', AdsRoute)
 app.use('/games', GamesRoute)
-
+app.use((request, response, next) => {
+    response.status(HttpCodes.NotFound).send()
+})
 
 app.listen(process.env.PORT || 5000)
